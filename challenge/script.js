22af43deb94c7edeef5e8071f72be13966fc3036
Sago21 <jsgaravito90@misena.edu.co>
2021-10-06 22:30:36 -0500

    Ejercicio JS

---
+++ challenge/script.js
@@ -0,0 +1,21 @@
+var socialMedia = {
+  facebook : 'http://facebook.com',
+  twitter: 'http://twitter.com',
+  flickr: 'http://flickr.com',
+  youtube: 'http://youtube.com'
+};
+
+
+// function iconos () {
+  
+   ico = document.querySelectorAll('.socialmediaicons');
+
+
+   // for () { 
+document.write('<a href="' + socialMedia.facebook +'"><img src="images/facebook.png"></a>'),
+document.write('<a href="' + socialMedia.twitter +'"><img src="images/twitter.png"></a>'),
+document.write('<a href="' + socialMedia.flickr +'"><img src="images/flickr.png"></a>'),
+document.write('<a href="' + socialMedia.youtube +'"><img src="images/youtube.png"></a>');
+// }
+
+// }
